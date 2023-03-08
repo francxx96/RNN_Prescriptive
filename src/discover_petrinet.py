f2a216aa43de252a28aa2ff205acce4e109763ed
Francesco <friva.job@outlook.com>
2023-03-08 11:16:01 +0100

    Bugfix

--- src/discover_petrinet.py
+++ src/discover_petrinet.py
@@ -3,7 +3,7 @@ import pm4py
 
 from commons import shared_variables as shared
 
-log_name = 'Synthetic log labelled'
+log_name = 'sepsis_cases_1'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,18 +12,29 @@ if __name__ == "__main__":
 
     log_path = shared.log_folder / args.log
 
+    log_settings = shared.log_settings[log_name]
+    activity_key = log_settings['act_name_key']
+    timestamp_key = log_settings['time_key']
+    case_id_key = 'case:'+log_settings['trace_name_key']
+
     xes_log = pm4py.read_xes(str(log_path))
-    net, im, fm = pm4py.discover_petri_net_heuristics(xes_log)
+    net, im, fm = pm4py.discover_petri_net_heuristics(log=xes_log,
+                                                      activity_key=activity_key,
+                                                      timestamp_key=timestamp_key,
+                                                      case_id_key=case_id_key)
 
-    alignments = pm4py.conformance_diagnostics_alignments(xes_log, net, im, fm)
+    alignments = pm4py.conformance_diagnostics_alignments(xes_log, net, im, fm,
+                                                          activity_key=activity_key,
+                                                          timestamp_key=timestamp_key,
+                                                          case_id_key=case_id_key)
 
-    print(f'th_fitness,heuristics % compliant traces')
+    print(f'th_fitness,% compliant traces')
     for i in range(1, 101):
         th = i / 100
         num_compliant_traces = len([alignment for alignment in alignments if alignment['fitness'] >= th])
-        log_size = xes_log['case:concept:name'].nunique()
+        log_size = xes_log[case_id_key].nunique()
         print(f'{th},{num_compliant_traces / log_size * 100}')
 
     # pm4py.view_petri_net(net, im, fm)
-    pm4py.save_vis_petri_net(net, im, fm, shared.pn_folder / (log_name+'_heuristics.jpg'))
-    pm4py.write_pnml(net, im, fm, shared.pn_folder / (log_name+'_heuristics.pnml'))
+    pm4py.save_vis_petri_net(net, im, fm, shared.pn_folder / (log_name+'.jpg'))
+    pm4py.write_pnml(net, im, fm, shared.pn_folder / (log_name+'.pnml'))
